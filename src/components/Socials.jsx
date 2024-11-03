8709543826a2048d10fc6f498bba04af893ed80a
Haris <github.actualharris@slalias.nl>
2024-11-04 00:02:21 +0530

    fix typos

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -13,7 +13,7 @@ const Socials = ()=>{
         <Profile/>
         <div className="col-lg-6 d-flex flex-column align-items-center text-center">
             <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 heading">Say Hello!</h1>
-            <p className="lead describe">Looking to connect? You can find me on various platforms! Follow me on LinkedIn, and feel free to reach out via email. I also showcase my coding projects on GitHub. If you have any <span className="imp">job oppurtunities</span>, please reach out — I'd love to hear from you!</p>
+            <p className="lead describe">Looking to connect? You can find me on various platforms! Follow me on LinkedIn, and feel free to reach out via email. I also showcase my coding projects on GitHub. If you have any <span className="imp">job opportunities</span>, please reach out — I'd love to hear from you!</p>
             <Navigation/>
 
 <hr/>
