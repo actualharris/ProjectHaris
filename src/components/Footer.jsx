516977ca6a388685c4c64ca74325a6ea369cf079
Haris <github.actualharris@slalias.nl>
2024-11-03 23:55:16 +0530

    fix navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from "react";
 
 const Footer = ()=>{
+
+  const year = new Date().getFullYear();
+
     return (
       <Fragment>
         <div className="container">
           <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 d-flex align-items-center">
-            <span className="mb-0 mb-md-0 text-body-secondary hey">© 2024 mohammed haris</span>
+            <span className="mb-0 mb-md-0 text-body-secondary hey">Copyright ⓒ {year} mohammed haris</span>
           </div>
       
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Link } from 'react-router-dom';
 import Navigation from "./Navigation";
 import Profile from "./Profile";
 
@@ -14,12 +15,10 @@ const Home = () => {
                             <p className="tiny">you've reached,</p>
                             <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 heading">Mohammed Haris</h1>
                         </div>
-                        {/* <p className="tiny">you've reached,</p>
-                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 heading">Mohammed Haris</h1> */}
                         <p className="lead describe">
                             Hello and welcome! <b>I'm</b>, a <span className="imp">B.Tech graduate, Web Developer</span>, and an aspiring <span className="imp">Cybersecurity Researcher</span>. 
-                            I'm thrilled to present my <a href="/projects" style={{ color: 'black', textDecoration: 'none' }}><span className="imp">Projects</span></a>, which highlight my passion for creating 
-                            innovative and impactful solutions. Be sure to check out the <a href="/about" style={{ color: 'black', textDecoration: 'none' }}><span className="imp">About Me </span></a> section to dive deeper 
+                            I'm thrilled to present my <Link to="/projects" style={{ color: 'black', textDecoration: 'none' }}><span className="imp">Projects</span></Link>, which highlight my passion for creating 
+                            innovative and impactful solutions. Be sure to check out the <Link to="/about" style={{ color: 'black', textDecoration: 'none' }}><span className="imp">About Me </span></Link> section to dive deeper 
                             into my journey and goals in both web development and cybersecurity.
                         </p>
                         <Navigation/>
