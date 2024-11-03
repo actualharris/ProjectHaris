a2103739a4049c2b2c6d9815988b6b8ea08b5875
Haris <github.actualharris@slalias.nl>
2024-11-03 19:52:48 +0530

    fix

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,31 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-import Home from "./Home"
-import About from "./About"
+import Home from "./Home";
+import About from "./About";
 import Socials from './Socials';
 import Projects from './Projects';
-
-import Footer from "./Footer"
+import Footer from "./Footer";
 
 function AppRoutes() {
-
-
     return (
-      <>
-      <Router>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/projects" element={<Projects />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/socials" element={<Socials />} />
-                  {/* Add more routes as needed */}
-              </Routes>
-              <Footer /> {/* Placed outside Routes */}
+        <Router basename="/ProjectHaris/">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/socials" element={<Socials />} /> 
+
+            </Routes>
+            <Footer />
         </Router>
-      </>
-    )
-  }
-  
-  export default AppRoutes;
-  
+    );
+}
+
+export default AppRoutes;
